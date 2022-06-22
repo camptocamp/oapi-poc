ffbbe459ce8889c78d2f7e78157fee279d2c8bea
Balthasar Teuscher <balthasar.teuscher@camptocamp.com>
2022-06-22 16:43:17 +0200

    Load current observations as features

--- ogcapi-poc/src/register.rs
+++ ogcapi-poc/src/register.rs
@@ -2,9 +2,9 @@ use std::path::Path;
 
 use aws_sdk_s3::model::ObjectCannedAcl;
 use chrono::{SecondsFormat, Utc};
-use serde_json::{Map, json};
+use serde_json::{json, Map, Value};
 
-use ogcapi_drivers::{postgres::Db, s3::S3, FeatureTransactions};
+use ogcapi_drivers::{postgres::Db, s3::S3, CollectionTransactions, FeatureTransactions};
 use ogcapi_types::{
     common::{media_type::JSON, Crs},
     stac::Asset,
@@ -46,13 +46,18 @@ async fn ingest(key: &str, db: &Db, s3: &S3) -> anyhow::Result<()> {
     // Collection id (uuid)
     let collection_id = target.split('/').next().unwrap_or_default();
 
-    // Create asset
-    let mut asset = Asset::new(format!("{AWS_S3_BUCKET_BASE}/{target}"));
-    
-    asset.roles = vec!["data".to_string()];
-
+    // Asset id (defaults to file name)
     let p = Path::new(target);
 
+    let asset_id = match collection_id {
+        "e2e5132c-85df-417a-8706-f75068d4937e" => "meteoswiss.radar.precip",
+        _ => p.file_name().unwrap_or_default().to_str().unwrap(),
+    };
+
+    // Create asset
+    let mut asset = Asset::new(format!("{AWS_S3_BUCKET_BASE}/{target}"));
+
+    asset.roles = vec!["data".to_string()];
     asset.r#type = match p.extension().unwrap_or_default().to_str() {
         Some("json") => Some(JSON.to_string()),
         Some("csv") => Some("text/csv".to_string()),
@@ -61,12 +66,48 @@ async fn ingest(key: &str, db: &Db, s3: &S3) -> anyhow::Result<()> {
         _ => None,
     };
 
-    // Asset id (defaults to file name)
-    let asset_id = match collection_id {
-        "e2e5132c-85df-417a-8706-f75068d4937e" => "meteoswiss.radar.precip",
-        _ => p.file_name().unwrap_or_default().to_str().unwrap(),
-    };
+    // Copy object
+    s3.client
+        .copy_object()
+        .copy_source(format!("{AWS_S3_BUCKET}/{key}"))
+        .bucket(AWS_S3_BUCKET)
+        .key(target)
+        .acl(ObjectCannedAcl::PublicRead)
+        .send()
+        .await?;
 
+    // Update collection/item
+    if collection_id == "0a62455f-c39c-4084-bd54-36ee2192d3af" {
+        asset_to_collection(collection_id, asset_id, asset, db).await?;
+        if key.ends_with("ch.meteoschweiz.messwerte-lufttemperatur-10min_en.json") {
+            load_items_from_object(key, collection_id, db, s3).await?;
+        }
+    } else {
+        asset_to_item(collection_id, asset_id, asset, db).await?;
+    }
+
+    // Cleanup
+    s3.delete_object(AWS_S3_BUCKET, key).await?;
+    if collection_id == "e2e5132c-85df-417a-8706-f75068d4937e" {
+        let resp = s3.list_objects(AWS_S3_BUCKET, Some(collection_id)).await?;
+
+        for object in resp.contents().unwrap_or_default() {
+            let key = object.key().unwrap_or_default();
+            if key != target {
+                s3.delete_object(AWS_S3_BUCKET, key).await?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+async fn asset_to_item(
+    collection_id: &str,
+    asset_id: &str,
+    asset: Asset,
+    db: &Db,
+) -> anyhow::Result<()> {
     // Get item (skip key if no mapping)
     let item_id = match collection_id {
         "0a62455f-c39c-4084-bd54-36ee2192d3af" => "messwerte-lufttemperatur-10min",
@@ -86,30 +127,92 @@ async fn ingest(key: &str, db: &Db, s3: &S3) -> anyhow::Result<()> {
     // Add/update asset
     item.assets.insert(asset_id.to_string(), asset);
 
-    // Copy object
-    s3.client
-        .copy_object()
-        .copy_source(format!("{AWS_S3_BUCKET}/{key}"))
-        .bucket(AWS_S3_BUCKET)
-        .key(target)
-        .acl(ObjectCannedAcl::PublicRead)
-        .send()
-        .await?;
-
     // Write item
     db.update_feature(&item).await?;
 
-    // Cleanup
-    s3.delete_object(AWS_S3_BUCKET, key).await?;
-    if collection_id == "e2e5132c-85df-417a-8706-f75068d4937e" {
-        let resp = s3.list_objects(AWS_S3_BUCKET, Some(collection_id)).await?;
+    Ok(())
+}
 
-        for object in resp.contents().unwrap_or_default() {
-            let key = object.key().unwrap_or_default();
-            if key != target {
-                s3.delete_object(AWS_S3_BUCKET, key).await?;
+async fn asset_to_collection(
+    collection_id: &str,
+    asset_id: &str,
+    asset: Asset,
+    db: &Db,
+) -> anyhow::Result<()> {
+    // Get collection
+    let mut collection = db.read_collection(collection_id).await?;
+
+    // Add/update asset
+    collection.assets.insert(asset_id.to_string(), asset);
+
+    // Write ollection
+    db.update_collection(&collection).await?;
+
+    Ok(())
+}
+
+async fn load_items_from_object(
+    key: &str,
+    collection_id: &str,
+    db: &Db,
+    s3: &S3,
+) -> anyhow::Result<()> {
+    // Load data
+    let resp = s3.get_object(AWS_S3_BUCKET, key).await?;
+    let data = resp.body.collect().await?.into_bytes();
+    let geojson_str = std::str::from_utf8(&data)?;
+    let geojson = geojson_str.parse::<geojson::GeoJson>()?;
+
+    match geojson {
+        geojson::GeoJson::FeatureCollection(mut fc) => {
+            let mut tx = db.pool.begin().await?;
+
+            sqlx::query(&format!(r#"TRUNCATE TABLE items."{}""#, collection_id))
+                .execute(&mut tx)
+                .await?;
+
+            for (i, feature) in fc.features.iter_mut().enumerate() {
+                let id = if let Some(id) = &feature.id {
+                    match id {
+                        geojson::feature::Id::String(id) => id.to_owned(),
+                        geojson::feature::Id::Number(id) => id.to_string(),
+                    }
+                } else {
+                    i.to_string()
+                };
+
+                if let Some(properties) = feature.properties.as_mut() {
+                    properties.remove("description");
+                    let datetime = properties.get("reference_ts").cloned().unwrap_or_else(|| {
+                        json!(Utc::now().to_rfc3339_opts(SecondsFormat::Secs, true))
+                    });
+                    properties.insert("datetime".to_string(), datetime);
+                }
+
+                sqlx::query(&format!(
+                    r#"
+                    INSERT INTO items."{}" (
+                        id,
+                        properties,
+                        geom
+                    ) VALUES (
+                        $1,
+                        $2,
+                        ST_Transform(ST_SetSRID(ST_GeomFromGeoJSON($3), 2056), 4326)
+                    )
+                    "#,
+                    collection_id
+                ))
+                .bind(id)
+                .bind(Value::from(feature.properties.take().unwrap()))
+                .bind(feature.geometry.take().unwrap().value.to_string())
+                .execute(&mut tx)
+                .await?;
             }
+
+            tx.commit().await?;
         }
+        _ => unimplemented!(),
     }
 
     Ok(())
